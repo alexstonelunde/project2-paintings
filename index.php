22b6365addbcb7912fff8e4c806593145d852643
astonelunde <alexlunde@gmail.com>
2020-02-03 15:55:14 +0000

    continuing to flesh out main page structure

--- index.php
+++ index.php
@@ -3,27 +3,47 @@
         <title>
             A collection of impressionist paintings from the Met that I like.
         </title>
+            <link rel = "stylesheet" type = "text/css" href = "css/normalize.css" />
+            <link rel = "stylesheet" type = "text/css" href = "css/styles.css" />
     </head>
 
 <body>
     <nav>
         <div class="navContainer">
-            <div class="navLogo">
+            <div class="navElement">
                 SAMPLE TEXT
             </div>
 
-            <div class="navCollection">
+            <div class="navElement">
                 SAMPLE TEXT
             </div>
 
-            <div class="navAddToCollection">
+            <div class="navElement">
                 SAMPLE TEXT
             </div>
         </div>
     </nav>
 
     <section class="main">
+        <div class="contentContainer">
+            <div class="contentElement">
+                <div class="contentElementPicture">
+                    picture
+                </div>
 
+                <div class="contentElementText">
+                    description
+                </div>
+            </div>
+
+            <div class="contentElement">
+
+            </div>
+
+            <div class="contentElement">
+
+            </div>
+        </div>
     </section>
 </body>
 </html>
