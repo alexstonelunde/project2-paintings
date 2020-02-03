023898f4a9a571f2a5bf0ed4ca8b932aa8dee172
astonelunde <alexlunde@gmail.com>
2020-02-03 15:24:47 +0000

    started building collections page skeleton

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="en">
+    <head>
+        <title>
+            A collection of impressionist paintings from the Met that I like.
+        </title>
+    </head>
+
+<body>
+    <nav>
+        <div class="navContainer">
+            <div class="navLogo">
+                SAMPLE TEXT
+            </div>
+
+            <div class="navCollection">
+                SAMPLE TEXT
+            </div>
+
+            <div class="navAddToCollection">
+                SAMPLE TEXT
+            </div>
+        </div>
+    </nav>
+
+    <section class="main">
+
+    </section>
+</body>
+</html>
